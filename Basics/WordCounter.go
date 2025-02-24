306209fd97790ba7dbd4d60e373a7c60cc2fc10d
snehal-sharma <snehal931994@gmail.com>
2025-02-24 18:17:16 +0530

    Update WordCounter.go

--- Basics/WordCounter.go
+++ Basics/WordCounter.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/tour/wc"
 	"strings"
 )
 
 func WordCount(s string) map[string]int {
 	words :=strings.Split(s," ")
-	fmt.Println(words)
 	WordCounter :=make(map[string]int,len(words))
 	for _, word := range words{
 		value, ok := WordCounter[word]
@@ -20,4 +18,9 @@ func WordCount(s string) map[string]int {
 	
 	}
 	return WordCounter
+}
+
+func main() {
+	CounterMap := WordCount("I am learning Go!")
+	fmt.Println(CounterMap)
 }
