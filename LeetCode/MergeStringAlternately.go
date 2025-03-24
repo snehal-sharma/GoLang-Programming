67580afb3e794a529831d2eca4ae73035d8147a9
snehal-sharma <snehal931994@gmail.com>
2025-03-24 20:49:10 +0530

    Update MergeStringAlternately.go

--- LeetCode/MergeStringAlternately.go
+++ LeetCode/MergeStringAlternately.go
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/merge-strings-alternately/
 */
 
+//Better Runtime consumes less memory.
 func mergeAlternately(word1 string, word2 string) string {
     merged := make([]byte, 0, len(word1)+len(word2))
     for i:=0;i<len(word1) || i<len(word2);i++{
