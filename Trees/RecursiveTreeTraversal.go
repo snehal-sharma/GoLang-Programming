5c64b165cb1ac1ed76957b8a7d4984400cdf0935
snehal-sharma <snehal931994@gmail.com>
2025-04-16 19:46:47 +0530

    Update RecursiveTreeTraversal.go

--- Trees/RecursiveTreeTraversal.go
+++ Trees/RecursiveTreeTraversal.go
@@ -1,4 +1,3 @@
-
 package main
 
 import "fmt"
@@ -11,13 +10,36 @@ type BinaryTreeNode struct {
 
 func PreOrder(root *BinaryTreeNode) {
 	if root != nil {
-		fmt.Println(root.Data)
+		fmt.Printf("\t%d", root.Data)
 		PreOrder(root.Left)
 		PreOrder(root.Right)
 	}
 }
 
+func InOrder(root *BinaryTreeNode) {
+	if root != nil {
+		InOrder(root.Left)
+		fmt.Printf("\t%d", root.Data)
+		InOrder(root.Right)
+	}
+}
+
+func PostOrder(root *BinaryTreeNode) {
+	if root != nil {
+		PostOrder(root.Left)
+		PostOrder(root.Right)
+		fmt.Printf("\t%d", root.Data)
+	}
+}
+
 func main() {
 	binaryTree := BinaryTreeNode{Data: 1, Left: &BinaryTreeNode{Data: 2, Left: &BinaryTreeNode{Data: 4}, Right: &BinaryTreeNode{Data: 5}}, Right: &BinaryTreeNode{Data: 3, Left: &BinaryTreeNode{Data: 6}, Right: &BinaryTreeNode{Data: 7}}}
+	fmt.Println("PreOrder Tree Traversal : ")
 	PreOrder(&binaryTree)
+
+	fmt.Println("\nInOrder Tree Traversal : ")
+	InOrder(&binaryTree)
+
+	fmt.Println("\nPostOrder Tree Traversal : ")
+	PostOrder(&binaryTree)
 }
