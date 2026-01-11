0e2925862e64289f3a0188a90b108f6f34914532
snehal-sharma <snehal931994@gmail.com>
2026-01-11 20:38:03 +0530

    Implement odd and even number printing with goroutines

---
+++ Channels/OddEven.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// Channels for signaling between odd and even goroutines
+	oddChan := make(chan struct{})  // Use struct{} for empty signal
+	evenChan := make(chan struct{})
+	
+	var wg sync.WaitGroup
+	wg.Add(2)
+	const limit = 10
+
+	// Odd number goroutine
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= limit; i += 2 {
+			<-oddChan // Wait for signal to print odd
+			fmt.Println("Odd:", i)
+			evenChan <- struct{}{} // Signal even to print
+		}
+		// Close even channel to signal the end of sequence
+		close(evenChan)
+	}()
+
+	// Even number goroutine
+	go func() {
+		defer wg.Done()
+		// Range over the even channel to receive signals until it is closed
+		for i := 2; i <= limit; i += 2 {
+			<-evenChan // Wait for signal to print even
+			fmt.Println("Even:", i)
+			oddChan <- struct{}{} // Signal odd to print
+		}
+	}()
+
+	// Initial signal to start the odd goroutine
+	oddChan <- struct{}{}
+
+	// Wait for both goroutines to finish
+	wg.Wait()
+}
