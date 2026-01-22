9232cfb0c5111c4bcb5e8ef32a62ac5e5fac3077
snehal-sharma <snehal931994@gmail.com>
2026-01-22 21:57:51 +0530

    Implement Selection Sort algorithm in Go

---
+++ Sorting/SelectionSort.go
@@ -0,0 +1,25 @@
+
+package main
+
+import "fmt"
+
+func SelectionSort(array []int) {
+	for i := 0; i < len(array)-1; i++ {
+		min := i
+		for j := i + 1; j < len(array); j++ {
+			if array[j] < array[min] {
+				min = j
+			}
+		}
+		temp := array[i]
+		array[i] = array[min]
+		array[min] = temp
+	}
+}
+
+func main() {
+	array := []int{3, 6, 8, 1, 7, 4, 9, 2, 5}
+	fmt.Println("Array Before Sorting : ", array)
+	SelectionSort(array[:])
+	fmt.Println("Array After Sorting : ", array)
+}
