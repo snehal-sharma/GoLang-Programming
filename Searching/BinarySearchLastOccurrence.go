78fed55d6e3dda0bf953c895e7ab4c971bff0693
snehal-sharma <snehal931994@gmail.com>
2026-01-29 21:29:11 +0530

    Add BinarySearchLastOccurrence function
    Implement binary search to find the last occurrence of a given number in a sorted array.

---
+++ Searching/BinarySearchLastOccurrence.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func BinarySearchLastOccurrence(arr []int, low, high, data int) int {
+	if high >= low {
+		mid := low + (high-low)/2
+		fmt.Printf("\nLow : %d Mid : %d High : %d", low, mid, high)
+
+		if (mid == high && arr[mid] == data) || (arr[mid+1] > data && arr[mid] == data) {
+			return mid
+		} else if arr[mid] <= data {
+			fmt.Println("\nArray : ", arr[mid+1:])
+			return BinarySearchLastOccurrence(arr, mid+1, high, data)
+		} else {
+			fmt.Println("\nArray : ", arr[low:mid])
+			return BinarySearchLastOccurrence(arr, low, mid-1, data)
+		}
+	}
+	return -1
+}
+
+func main() {
+	array := []int{1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 8, 9}
+	found := BinarySearchLastOccurrence(array, 0, len(array)-1, 7)
+	fmt.Println("\nFound last occurrence at : ", found)
+}
