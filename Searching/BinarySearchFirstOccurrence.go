b5a443ecef4e51b09d142e20a27ccdeb8cfcc9b0
snehal-sharma <snehal931994@gmail.com>
2026-01-29 21:31:53 +0530

    Fix binary search for first occurrence of a number

--- Searching/BinarySearchFirstOccurrence.go
+++ Searching/BinarySearchFirstOccurrence.go
@@ -10,19 +10,19 @@ func BinarySearchFirstOccurrence(arr []int, low, high, data int) int {
 
 		if (mid == low && arr[mid] == data) || (arr[mid-1] < data && arr[mid] == data) {
 			return mid
-		} else if arr[mid] < data {
-			fmt.Println("\nArray : ", arr[mid+1:])
-			return BinarySearchFirstOccurrence(arr, mid+1, high, data)
-		} else {
+		} else if arr[mid] >= data {
 			fmt.Println("\nArray : ", arr[low:mid])
 			return BinarySearchFirstOccurrence(arr, low, mid-1, data)
+		} else {
+			fmt.Println("\nArray : ", arr[mid+1:])
+			return BinarySearchFirstOccurrence(arr, mid+1, high, data)
 		}
 	}
 	return -1
 }
 
 func main() {
-	array := []int{1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 9}
-	found := BinarySearchFirstOccurrence(array, 0, len(array)-1, 4)
+	array := []int{1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 8, 9}
+	found := BinarySearchFirstOccurrence(array, 0, len(array)-1, 5)
 	fmt.Println("\nFound first occurrence at : ", found)
 }
