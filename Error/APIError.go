39a775cf667e3460ab84dc8560b673b24d1a3644
snehal-sharma <snehal931994@gmail.com>
2026-02-07 16:28:32 +0530

    Create APIError.go

---
+++ Error/APIError.go
@@ -0,0 +1,35 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type APIError struct {
+	code    int
+	message string
+	err     error
+}
+
+func (apierr *APIError) Error() string {
+	return fmt.Sprintf("Code : %d Message :%s Error:%v", apierr.code, apierr.message, apierr.err)
+}
+
+func ThrowAPIError() error {
+	return &APIError{
+		code:    404,
+		message: "Not Found",
+		err:     nil,
+	}
+}
+func main() {
+	var apiErr *APIError
+	err := ThrowAPIError()
+	if err != nil {
+		if errors.As(err, &apiErr) {
+			fmt.Println(err)
+		}
+	}
+}
