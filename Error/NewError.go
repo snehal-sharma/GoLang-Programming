78cd6e4a9d1f9560219ed3cd85ec91fafd143825
snehal-sharma <snehal931994@gmail.com>
2026-02-07 16:00:30 +0530

    Create NewError.go

---
+++ Error/NewError.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrorNotFound = errors.New("Item not found")
+
+func getItem(id int) error {
+	if id == 0 {
+		return ErrorNotFound
+	}
+	return nil
+}
+
+func main() {
+	err := getItem(0)
+	if err == ErrorNotFound {
+		fmt.Println(err)
+	}
+}
