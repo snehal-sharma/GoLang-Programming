2e50afd951fdc9116de8038a4e9473537295a9d9
snehal-sharma <snehal931994@gmail.com>
2026-02-08 18:16:44 +0530

    Create FactoryPattern.go

---
+++ DesginPatterns/FactoryPattern.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Payment interface {
+	Pay(float64)
+}
+
+type CreditCard struct{}
+
+func (cc CreditCard) Pay(amount float64) {
+	fmt.Println("Payment through Credit Card :", amount)
+}
+
+type UPI struct{}
+
+func (upi UPI) Pay(amount float64) {
+	fmt.Println("Payment through UPI :", amount)
+}
+
+func PaymentFactory(mode string) Payment {
+	switch mode {
+	case "CreditCard":
+		return CreditCard{}
+	case "UPI":
+		return UPI{}
+	default:
+		fmt.Println("Unauthorized payment mode")
+		return nil
+	}
+}
+
+func main() {
+	mode := PaymentFactory("UPI")
+	mode.Pay(64)
+}
