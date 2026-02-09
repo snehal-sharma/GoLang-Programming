b3566102c466ec73bca7cdcb054ad3bbfd42ccc9
snehal-sharma <snehal931994@gmail.com>
2026-02-09 18:22:06 +0530

    Update ContextWithTimeOut.go

--- Study Material/Context/ContextWithTimeOut.go
+++ Study Material/Context/ContextWithTimeOut.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	go performTask(ctx)
