ee7e14c01e6759adc6003f122a59df01335c2fe0
snehal-sharma <snehal931994@gmail.com>
2026-02-09 12:19:12 +0530

    Create Goroutines.go

---
+++ Study Material/Concurrency/Goroutines.go
@@ -0,0 +1,4 @@
+* A goroutine is a lightweight concurrent execution unit managed by the Go runtime Scheduler. 
+* The stacks start small with an initial stack size (usually 2KB or 4KB) so they are cheap and grow by allocating (and freeing) heap storage as required.
+  As a result, Go programs can spawn thousands of goroutines with relatively low overhead.
+* Goroutines are multiplexed onto smaller number of OS threads in an interleaved manner so if one should block, such as while waiting for I/O, others continue to run.
